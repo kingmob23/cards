64bacfdd82c14610b6863719b1ff9d07b9023f53
latte_coder <nogasobaki23@gmail.com>
2023-06-22 02:35:26 +0300

    added funky red hand for future card holding

--- src/mainscene.js
+++ src/mainscene.js
@@ -1,9 +1,13 @@
 import Phaser from "phaser";
 import background from "./assets/background.jpg";
-
+import handImage from "./assets/hand.png";
 
 const gameWidth = 1024;
 const gameHeight = 576;
+
+var hand = [];
+var table = [];
+var deck = [];
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
@@ -12,14 +16,54 @@ export default class MainScene extends Phaser.Scene {
 
   preload() {
     this.load.image("background", background);
+    this.load.image("handImage", handImage);
   }
 
   create() {
+    // var tableSprite = this.add.sprite(400, 300, "tableImage");
+    // var deckSprite = this.add.sprite(600, 400, "deckImage");
+
+    // var cardRun = { id: 1, name: "Card Run", image: "cardRunImage" };
+    // var cardSleep = { id: 2, name: "Card Sleep", image: "cardSleepImage" };
+    // var cardWink = { id: 3, name: "Card Wink", image: "cardWinkImage" };
+
+    // hand.push(cardRun);
+    // hand.push(cardSleep);
+    // hand.push(cardWink);
+
+    // hand.forEach(function(card) {
+    //   var cardSprite = this.add.sprite(0, 0, card.image);
+    //   cardSprite.setInteractive();
+
+    //   cardSprite.on(
+    //     "pointerdown",
+    //     function(pointer) {
+    //       console.log("Вы выбрали карту:", card.name);
+    //     },
+    //     this
+    //   );
+
+    //   var cardOffsetX = 50;
+    //   var cardOffsetY = 0;
+    //   var cardSpacing = 20;
+
+    //   cardSprite.x =
+    //     handSprite.x -
+    //     hand.length * cardOffsetX +
+    //     cardOffsetX * hand.indexOf(card) +
+    //     cardSpacing * hand.indexOf(card);
+    //   cardSprite.y = handSprite.y + cardOffsetY;
+    // }, this);
+
     const bg = this.add.image(gameWidth / 2, gameHeight / 2, "background");
 
     const scaleX = gameWidth / bg.width;
     const scaleY = gameHeight / bg.height;
     const scale = Math.max(scaleX, scaleY);
     bg.setScale(scale).setScrollFactor(0);
+
+    var handSprite = this.add.sprite(300, gameHeight - 150, "handImage").setScale(0.5);
+    handSprite.setAngle(-30);
+
   }
 }
